d3dfa89bf088f799e38e4553aa00d4cbe7eb4a7c
Jason Toy <jasontoy@gmail.com>
2015-12-14 17:03:54 -0600

    update test script

--- test.sh
+++ test.sh
@@ -11,6 +11,7 @@ else
 fi
 cd /home/ubuntu/experiment
 OUTPUT="$(date +%s)"
-/home/ubuntu/torch/install/bin/th /home/ubuntu/experiment/neural_style.lua -output_image /home/ubuntu/$OUTPUT.jpg $@ >> /home/ubuntu/logs 2>&1
+/home/ubuntu/torch/install/bin/th /home/ubuntu/experiment/sample.lua -gpuid -1 $@ > /home/ubuntu/$OUTPUT.txt   2>&1
+#/home/ubuntu/torch/install/bin/th /home/ubuntu/experiment/sample.lua > /home/ubuntu/$OUTPUT.txt $@ >> /home/ubuntu/logs 2>&1
 #/home/ubuntu/torch/install/bin/th /home/ubuntu/experiment/neural_style.lua -gpu 0 -print_iter 1 -num_iterations 10 -style_image /home/ubuntu/experiment/examples/inputs/escher_sphere.jpg -content_image $1 -output_image "/home/ubuntu/$OUTPUT.jpg"   > /dev/null 2>&1
-echo /home/ubuntu/$OUTPUT.jpg
+echo /home/ubuntu/$OUTPUT.txt
