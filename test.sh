01a670e50c178625e777f678df6b6da8bff19868
root <root@ip-172-31-6-67.ec2.internal>
2015-12-15 16:52:54 +0000

    fixing up sampling

--- test.sh
+++ test.sh
@@ -11,7 +11,9 @@ else
 fi
 cd /home/ubuntu/experiment
 OUTPUT="$(date +%s)"
-/home/ubuntu/torch/install/bin/th /home/ubuntu/experiment/sample.lua -gpuid -1 $@ > /home/ubuntu/$OUTPUT.txt   2>&1
+FILE="$(ls -t1 cv | head -1)"
+/home/ubuntu/torch/install/bin/th /home/ubuntu/experiment/sample.lua cv/$FILE -gpuid -1 $@ > /home/ubuntu/$OUTPUT.txt   2>&1
+#/home/ubuntu/torch/install/bin/th /home/ubuntu/experiment/sample.lua cv/$FILE -gpuid -1 $@
 #/home/ubuntu/torch/install/bin/th /home/ubuntu/experiment/sample.lua > /home/ubuntu/$OUTPUT.txt $@ >> /home/ubuntu/logs 2>&1
 #/home/ubuntu/torch/install/bin/th /home/ubuntu/experiment/neural_style.lua -gpu 0 -print_iter 1 -num_iterations 10 -style_image /home/ubuntu/experiment/examples/inputs/escher_sphere.jpg -content_image $1 -output_image "/home/ubuntu/$OUTPUT.jpg"   > /dev/null 2>&1
 echo /home/ubuntu/$OUTPUT.txt
