10a201e9baece10fb795e13ec2f5abbf243d7acf
Jason Toy <jasontoy@gmail.com>
2015-12-15 11:51:26 -0600

    fix test var

--- tests/testsuite.sh
+++ tests/testsuite.sh
@@ -1,5 +1,5 @@
 #docker build -t somatic/char-rnn .
 docker run  -e "AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID" -e "AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY"  -d -p 5000:5000 somatic/char-rnn python /home/ubuntu/somaticagent/web.py -s
-curl --fail -X POST -F prime_text="the meaning of life is "  http://127.0.0.1:5000/run
+curl --fail -X POST -F primetext="the meaning of life is "  http://127.0.0.1:5000/run
 #docker run  -e "AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID" -e "AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY"  -t -p 5000:5000 somatic/bat-country p
 
